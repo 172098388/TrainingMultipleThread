75e4c92ea7c838b2ca6e0f5d8e6846c7de824f88
172098388 <172098388@qq.com>
2016-08-01 08:30:12 +0800

    多线程加滚动条

---
+++ app/src/main/java/com/example/lsx/trainingmultiplethread/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.lsx.trainingmultiplethread;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView mImageView;
+    private Button mLoadImageButton;
+    private Button mToastButton;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.activity_main_image_view);
+        mLoadImageButton = (Button) findViewById(R.id.activity_main_load_image_button);
+        mToastButton = (Button) findViewById(R.id.activity_main_toast_button);
+        mProgressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
+
+        mLoadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LoadImageTask().execute();
+
+            }
+        });
+        mToastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "xxxxxxxxxxxxxxxx", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    class LoadImageTask extends AsyncTask<Void, Integer, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            for (int i = 1; i < 11; i++) {
+                sleep();
+                publishProgress(i * 10);
+            }
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+            return bitmap;
+        }
+
+        private void sleep() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            mImageView.setImageBitmap(bitmap);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+
+}
